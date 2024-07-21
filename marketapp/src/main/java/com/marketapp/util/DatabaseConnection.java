3a06e80cf93f23acee43d38547dcdc5a62e60ae3
Enes Burak Polat <102995609+EnesBurakPolat@users.noreply.github.com>
2024-07-22 01:28:24 +0300

    Update DatabaseConnection.java

--- marketapp/src/main/java/com/marketapp/util/DatabaseConnection.java
+++ marketapp/src/main/java/com/marketapp/util/DatabaseConnection.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class DatabaseConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/marketapp_db";
     private static final String USER = "root";
-    private static final String PASSWORD = "10851111";
+    private static final String PASSWORD = "yourpassword:)";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
